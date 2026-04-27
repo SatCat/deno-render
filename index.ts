594962ee1e8913cce9056873a37f780f2a327bd4
SatCat <39083134+SatCat@users.noreply.github.com>
2026-04-27 16:08:36 +1100

    Update index.ts add counter

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-//import { serve } from "https://deno.land/std@0.123.0/http/server.ts";
 const port = 10000;
+
+// global counter
+let hourlyCounter = 0;
+
+setInterval(() => {
+  hourlyCounter++;
+}, 3_600_000);
 
 const handler = (request: Request): Response => {
   let body = "Hello from Deno 2+!\n\n";
@@ -7,13 +13,15 @@ const handler = (request: Request): Response => {
   body += `Deno version: ${Deno.version.deno}\n`;
   body += `TypeScript version: ${Deno.version.typescript}\n`;
   body += `V8 engine version: ${Deno.version.v8}\n\n`;
+  body += `Counter: ${hourlyCounter}\n\n`;
   
   body += "Your user-agent is:\n\n";
   body += request.headers.get("user-agent") || "Unknown";
 
-  return new Response(body, { status: 200 });
+  return new Response(body, { 
+    status: 200,
+    headers: { "content-type": "text/plain; charset=utf-8" } 
+  });
 };
 
-//console.log(`HTTP webserver running on port ${port}.`);
-//await serve(handler, { hostname: "0.0.0.0", port: port });
 Deno.serve({ port, hostname: "0.0.0.0" }, handler);
