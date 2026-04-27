85e540ba2da7d7b40b3f673edd3dd31d52b17662
SatCat <39083134+SatCat@users.noreply.github.com>
2026-04-27 15:57:36 +1100

    Update index.ts - show versions

--- index.ts
+++ index.ts
@@ -2,13 +2,18 @@
 const port = 10000;
 
 const handler = (request: Request): Response => {
-  let body = "Hello from Deno!\n\n";
+  let body = "Hello from Deno 2+!\n\n";
+  // Show versions
+  body += `Deno version: ${Deno.version.deno}\n`;
+  body += `TypeScript version: ${Deno.version.typescript}\n`;
+  body += `V8 engine version: ${Deno.version.v8}\n\n`;
+  
   body += "Your user-agent is:\n\n";
   body += request.headers.get("user-agent") || "Unknown";
 
   return new Response(body, { status: 200 });
 };
 
-console.log(`HTTP webserver running on port ${port}.`);
+//console.log(`HTTP webserver running on port ${port}.`);
 //await serve(handler, { hostname: "0.0.0.0", port: port });
 Deno.serve({ port, hostname: "0.0.0.0" }, handler);
